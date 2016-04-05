18760e50cc67fb7b537fbe458508dd44cbe67d9a
toby ealden <toby.ealden@gmail.com>
2016-04-05 12:53:57 +0100

    rationalise with utils in nqm-tbx

--- index.js
+++ index.js
@@ -5,30 +5,18 @@
 module.exports = (function() {
   "use strict";
 
-  var _ = require("lodash");
-
   return {
     loadConfig: require("./lib/loadConfig"),
     flattenJSON: require("./lib/flatten"),
-    buildDataKey: function(fieldPaths, params) {
-      /*
-       * Extract a dictionary of values from params corresponding to the given array of field paths,
-       * where paths use the dot notation, e.g. address.postcode.
-       */
-      var errLog = require("debug")("nqm-utils:buildDataKey:error");
-      var parser = require("nqm-mongo-parse");
-      var keys = {};
-      _.forEach(fieldPaths, function(f) {
-        // Get the field path.
-        var key = f.asc || f.desc;
-        var pointers = parser.DotNotationPointers(params, key);
-        if (pointers.length === 0 || !pointers[0].val) {
-          errLog("undefined key for field %s", key);
-        } else {
-          keys[key] = pointers[0].val;
-        }
-      });
-      return keys;
-    }
+    buildDataKey: require("./lib/buildDataKey"),
+    isEmailValid: function(address) { 
+      // TODO - improve this (use mailgun?)
+      return /^[A-Z0-9'.1234z_%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(address);
+    },
+    isHostNameValid: function(hostname) {
+      // From http://stackoverflow.com/a/106223
+      return /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(hostname);
+    },
+    isNumeric: function (n) { return !isNaN(parseFloat(n)) && isFinite(n); }
   }
 }());
