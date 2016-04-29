d603d29b14dd95777719e38de4c2b938830f4b32
toby ealden <toby.ealden@gmail.com>
2016-04-29 23:50:35 +0100

    combine email and tdx address for user id

--- index.js
+++ index.js
@@ -5,18 +5,42 @@
 module.exports = (function() {
   "use strict";
 
+  var isEmailValid = function(address) { 
+    // TODO - improve this (use mailgun?)
+    return /^[A-Z0-9'.1234z_%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(address);
+  };
+
+  var isHostNameValid = function(hostname) {
+    // From http://stackoverflow.com/a/106223
+    return /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(hostname);
+  };
+
   return {
     loadConfig: require("./lib/load-config"),
     flattenJSON: require("./lib/flatten"),
     buildDataKey: require("./lib/build-data-key"),
-    isEmailValid: function(address) { 
-      // TODO - improve this (use mailgun?)
-      return /^[A-Z0-9'.1234z_%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(address);
+    isEmailValid: isEmailValid,
+    isHostNameValid: isHostNameValid,
+    isNumeric: function (n) { return !isNaN(parseFloat(n)) && isFinite(n); },
+    makeTDXAccount: function(email, tdx) {
+      /*
+       * tdx accounts are stored in email/hostname format, e.g. toby.ealden@gmail.com/tdx.nqminds.com
+       */
+      return email + "/" + tdx;
     },
-    isHostNameValid: function(hostname) {
-      // From http://stackoverflow.com/a/106223
-      return /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(hostname);
-    },
-    isNumeric: function (n) { return !isNaN(parseFloat(n)) && isFinite(n); }
+    splitTDXAccount: function(username) {
+      /*
+       * tdx accounts are stored in email/hostname format, e.g. toby.ealden@gmail.com/tdx.nqminds.com
+       */
+      var result;
+      var split = username.split("/");
+      if (split.length === 2 && isEmailValid(split[0]) && (isHostNameValid(split[1]) || split[1].indexOf("localhost:") === 0)) {
+        result = {
+          email: split[0],
+          tdx: split[1]
+        };
+      }
+      return result;
+    }
   }
 }());
