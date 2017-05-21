b434ffdcb3c28433c0be5248d561fe2b57281977
toby ealden <toby.ealden@gmail.com>
2017-05-21 22:53:23 +0100

    add ability to fail buildDataKey if primary key is incomplete

--- src/build-data-key.js
+++ src/build-data-key.js
@@ -8,7 +8,7 @@ const errLog = debug("nqm-core-utils:build-data-key");
   * Extract a dictionary of values from params corresponding to the given array of field paths,
   * where paths use the dot notation, e.g. address.postcode.
   */
-const buildDataKey = function(fieldPaths, params) {
+const buildDataKey = function(fieldPaths, params, failIfIncomplete) {
   const keys = {};
   _.forEach(fieldPaths, function(f) {
     // Get the field path.
@@ -16,6 +16,9 @@ const buildDataKey = function(fieldPaths, params) {
     const pointers = parser.DotNotationPointers(params, key);
     if (pointers.length === 0 || typeof pointers[0].val === "undefined") {
       errLog("no key data for field '%s' in %j", key, params);
+      if (failIfIncomplete) {
+        throw new Error(`buildDataKey - incomplete primary key: no data for '${key}' in ${JSON.stringify(params)}`);
+      }
     } else {
       keys[key] = pointers[0].val;
     }
